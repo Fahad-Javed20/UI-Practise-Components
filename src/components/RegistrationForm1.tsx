e1836f6b915ff5ee1fc9b0c091c39499634f4a0a
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-22 00:11:46 +0500

    Update RegistrationForm1.tsx

--- src/components/RegistrationForm1.tsx
+++ src/components/RegistrationForm1.tsx
@@ -1,22 +1,109 @@
-
 const RegistrationForm1 = () => {
   return (
     <div className="flex justify-center">
+      <div className="bg-white w-9/12 h-10/12 grid grid-cols-2 ">
+        <div className="bg-white text-center rounded-tl-2xl rounded-bl-2xl">
+          <h1 className="pt-7 font-bold text-2xl">Registration</h1>
+          <div>
+            <input
+              className="mt-7 bg-gray-300 px-3 py-1 w-2/3"
+              type="text"
+              name=""
+              id=""
+              placeholder="Username"
+            />
+            <span className="relative">
+              <svg
+                className="size-5 absolute right-3 top-0"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"
+              >
+                <path d="M320 312C386.3 312 440 258.3 440 192C440 125.7 386.3 72 320 72C253.7 72 200 125.7 200 192C200 258.3 253.7 312 320 312zM290.3 368C191.8 368 112 447.8 112 546.3C112 562.7 125.3 576 141.7 576L498.3 576C514.7 576 528 562.7 528 546.3C528 447.8 448.2 368 349.7 368L290.3 368z" />
+              </svg>
+            </span>
+          </div>
 
-    <div className="bg-white w-10/12 h-10/12 grid grid-cols-2 ">
-        <div className="bg-white shadow-lg rounded-3xl">
-            <h1 className="pt-7 font-bold text-2xl">Registration</h1>
-            <input className="mt-7 bg-gray-300 px-3 py-1 w-2/3" type="text" name="" id="" placeholder="Username" />
-            <input className="mt-7 bg-gray-300 px-3 py-1 w-2/3" type="email" name="" id="" placeholder="Email" />
-            <input className="mt-7 bg-gray-300 px-3 py-1 w-2/3" type="password" name="" id="" placeholder="Password" />
+          <div>
+            <input
+              className="mt-7 bg-gray-300 px-3 py-1 w-2/3"
+              type="email"
+              name=""
+              id=""
+              placeholder="Email"
+            />
+            <span className="relative">
+              <svg
+                className="size-5 absolute right-3 top-0"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"
+              >
+                <path d="M112 128C85.5 128 64 149.5 64 176C64 191.1 71.1 205.3 83.2 214.4L291.2 370.4C308.3 383.2 331.7 383.2 348.8 370.4L556.8 214.4C568.9 205.3 576 191.1 576 176C576 149.5 554.5 128 528 128L112 128zM64 260L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 260L377.6 408.8C343.5 434.4 296.5 434.4 262.4 408.8L64 260z" />
+              </svg>
+            </span>
+          </div>
+          <div>
+            <input
+              className="mt-7 bg-gray-300 px-3 py-1 w-2/3"
+              type="password"
+              name=""
+              id=""
+              placeholder="Password"
+            />
+            <span className="relative">
+              <svg
+                className="size-5 absolute right-3 top-0"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 640"
+              >
+                <path d="M256 160L256 224L384 224L384 160C384 124.7 355.3 96 320 96C284.7 96 256 124.7 256 160zM192 224L192 160C192 89.3 249.3 32 320 32C390.7 32 448 89.3 448 160L448 224C483.3 224 512 252.7 512 288L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 288C128 252.7 156.7 224 192 224z" />
+              </svg>
+            </span>
+          </div>
+          <button className="mt-7 bg-violet-500 px-3 py-1 w-2/3 rounded">
+            Register
+          </button>
+          <p className="mt-2">Or register with Social platforms</p>
+
+          <div className="flex justify-center gap-4 mb-20 mt-5">
+            <svg
+              className="size-7 p-1 border rounded shadow-lg "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+            >
+              <path d="M564 325.8C564 467.3 467.1 568 324 568C186.8 568 76 457.2 76 320C76 182.8 186.8 72 324 72C390.8 72 447 96.5 490.3 136.9L422.8 201.8C334.5 116.6 170.3 180.6 170.3 320C170.3 406.5 239.4 476.6 324 476.6C422.2 476.6 459 406.2 464.8 369.7L324 369.7L324 284.4L560.1 284.4C562.4 297.1 564 309.3 564 325.8z" />
+            </svg>
+            <svg
+              className="size-7 p-1 border rounded shadow-lg "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+            >
+              <path d="M240 363.3L240 576L356 576L356 363.3L442.5 363.3L460.5 265.5L356 265.5L356 230.9C356 179.2 376.3 159.4 428.7 159.4C445 159.4 458.1 159.8 465.7 160.6L465.7 71.9C451.4 68 416.4 64 396.2 64C289.3 64 240 114.5 240 223.4L240 265.5L174 265.5L174 363.3L240 363.3z" />
+            </svg>
+            <svg
+              className="size-7 p-1 border rounded shadow-lg "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+            >
+              <path d="M237.9 461.4C237.9 463.4 235.6 465 232.7 465C229.4 465.3 227.1 463.7 227.1 461.4C227.1 459.4 229.4 457.8 232.3 457.8C235.3 457.5 237.9 459.1 237.9 461.4zM206.8 456.9C206.1 458.9 208.1 461.2 211.1 461.8C213.7 462.8 216.7 461.8 217.3 459.8C217.9 457.8 216 455.5 213 454.6C210.4 453.9 207.5 454.9 206.8 456.9zM251 455.2C248.1 455.9 246.1 457.8 246.4 460.1C246.7 462.1 249.3 463.4 252.3 462.7C255.2 462 257.2 460.1 256.9 458.1C256.6 456.2 253.9 454.9 251 455.2zM316.8 72C178.1 72 72 177.3 72 316C72 426.9 141.8 521.8 241.5 555.2C254.3 557.5 258.8 549.6 258.8 543.1C258.8 536.9 258.5 502.7 258.5 481.7C258.5 481.7 188.5 496.7 173.8 451.9C173.8 451.9 162.4 422.8 146 415.3C146 415.3 123.1 399.6 147.6 399.9C147.6 399.9 172.5 401.9 186.2 425.7C208.1 464.3 244.8 453.2 259.1 446.6C261.4 430.6 267.9 419.5 275.1 412.9C219.2 406.7 162.8 398.6 162.8 302.4C162.8 274.9 170.4 261.1 186.4 243.5C183.8 237 175.3 210.2 189 175.6C209.9 169.1 258 202.6 258 202.6C278 197 299.5 194.1 320.8 194.1C342.1 194.1 363.6 197 383.6 202.6C383.6 202.6 431.7 169 452.6 175.6C466.3 210.3 457.8 237 455.2 243.5C471.2 261.2 481 275 481 302.4C481 398.9 422.1 406.6 366.2 412.9C375.4 420.8 383.2 435.8 383.2 459.3C383.2 493 382.9 534.7 382.9 542.9C382.9 549.4 387.5 557.3 400.2 555C500.2 521.8 568 426.9 568 316C568 177.3 455.5 72 316.8 72zM169.2 416.9C167.9 417.9 168.2 420.2 169.9 422.1C171.5 423.7 173.8 424.4 175.1 423.1C176.4 422.1 176.1 419.8 174.4 417.9C172.8 416.3 170.5 415.6 169.2 416.9zM158.4 408.8C157.7 410.1 158.7 411.7 160.7 412.7C162.3 413.7 164.3 413.4 165 412C165.7 410.7 164.7 409.1 162.7 408.1C160.7 407.5 159.1 407.8 158.4 408.8zM190.8 444.4C189.2 445.7 189.8 448.7 192.1 450.6C194.4 452.9 197.3 453.2 198.6 451.6C199.9 450.3 199.3 447.3 197.3 445.4C195.1 443.1 192.1 442.8 190.8 444.4zM179.4 429.7C177.8 430.7 177.8 433.3 179.4 435.6C181 437.9 183.7 438.9 185 437.9C186.6 436.6 186.6 434 185 431.7C183.6 429.4 181 428.4 179.4 429.7z" />
+            </svg>
+            <svg
+              className="size-7 p-1 border rounded shadow-lg "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 640"
+            >
+              <path d="M196.3 512L103.4 512L103.4 212.9L196.3 212.9L196.3 512zM149.8 172.1C120.1 172.1 96 147.5 96 117.8C96 103.5 101.7 89.9 111.8 79.8C121.9 69.7 135.6 64 149.8 64C164 64 177.7 69.7 187.8 79.8C197.9 89.9 203.6 103.6 203.6 117.8C203.6 147.5 179.5 172.1 149.8 172.1zM543.9 512L451.2 512L451.2 366.4C451.2 331.7 450.5 287.2 402.9 287.2C354.6 287.2 347.2 324.9 347.2 363.9L347.2 512L254.4 512L254.4 212.9L343.5 212.9L343.5 253.7L344.8 253.7C357.2 230.2 387.5 205.4 432.7 205.4C526.7 205.4 544 267.3 544 347.7L544 512L543.9 512z" />
+            </svg>
+          </div>
         </div>
-        
-        <div className="bg-violet-500">2</div>
+
+        <div className="bg-violet-500 rounded-l-[120px] text-center ">
+<h1 className="mt-40 text-3xl text-white font-bold">Welcome Back!</h1>
+<p className="text-white mt-3 mb-3">Already have Account</p>
+<button className="border-2 border-white px-8 py-1 text-white rounded">Login</button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-
-
-export default RegistrationForm1
+export default RegistrationForm1;
